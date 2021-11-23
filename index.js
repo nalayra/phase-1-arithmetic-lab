61d86efffe70f5ea1a7a92f3222ca4863249df81
Nicholas Alayra <nalayra@hotmail.com>
2021-11-22 21:10:40 -0800

    commit tree 45331ec7112afca5c4a8e754a7c2c91d3d85410e

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 5
+let num4 = 9
+let mod = num4 % num3;
+
+let max = Math.max(1, 3, 20);
